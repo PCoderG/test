9e2d00f8154359e063e73c764ddaa9eced81fc60
PCoderG <92013953+PCoderG@users.noreply.github.com>
2021-12-21 15:55:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,23 @@ function submit(){
     }
     var length = name_array.length;
     console.log(length);
-    var display;
+    var display = [];
     for (j=0; j<length; j++){
         display.push("<h4>Name - "+name_array[j]+"</h4>");
         console.log(display);
     }
     document.getElementById("display_name_with_commas").innerHTML = display; 
+    var remove = display.join(" ");
+    console.log(remove);
+    document.getElementById("display_name_without_comma").innerHTML = remove;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display= "inline-blocks";
+}
+function sorting(){
+    name_array.sort();
+    console.log(name_array);
+    var dis=[];
+    for (var index = 0; index < length; index++) {
+        dis.push("<h4>Name" + display[index]+"</h4>")
+    }
 }
